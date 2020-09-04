1dce0427478d759f9907f15c1d47101b6c30a060
Sneha Motwani <snehamotwani11@gmail.com>
2020-09-04 20:50:29 +0530

    Output Pasted in Comments

--- BWConnection/Program.cs
+++ BWConnection/Program.cs
@@ -6,16 +6,23 @@ namespace BWConnection
     {
         static void Main(string[] args)
         {
-            MSSqlConnection sqlConnection = new MSSqlConnection("Server=myServerName\\myInstanceName;Database=myDataBase;User Id=myUsername;Password=myPassword", TimeSpan.FromSeconds(30));
+            /*
+               Sql connection is open
+               Sql connection is closed
+               Oracle Connection is open
+               Oracle Connection is closed
+               Connection String is null or empty
+             */
+            MSSqlConnection sqlConnection = new MSSqlConnection("Some Connection String for sql connection", TimeSpan.FromSeconds(30));
             sqlConnection.Open();
             sqlConnection.Close();
 
-            OracleDBConnection oracleDB1 = new OracleDBConnection("Server=myServerName\\myInstanceName;Database=myDataBase;User Id=myUsername;Password=myPassword", TimeSpan.FromSeconds(30));
+            OracleDBConnection oracleDB1 = new OracleDBConnection("Connection String for Oracle ", TimeSpan.FromSeconds(30));
             oracleDB1.Open();
             oracleDB1.Close();
 
-            OracleDBConnection oracleDB2 = new OracleDBConnection("", TimeSpan.FromSeconds(45));
-            oracleDB2.Open();
+            OracleDBConnection oracleDB2 = new OracleDBConnection("", TimeSpan.FromSeconds(45));    //null or empty case
+            //oracleDB2.Open();
 
             Console.ReadLine();
         }
